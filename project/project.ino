2f31a3e2dc635c69d97ba508ae9d3e5225890387
ulistein <95414602+ulistein@users.noreply.github.com>
2022-10-27 10:29:39 +0200

    implemented library for the matrix

--- project/project.ino
+++ project/project.ino
@@ -1,35 +1,17 @@
 #include <IRremote.h>
+#include "src/classes/Matrix/matrix.h"
 
-//define pins for 8*8
-  #define r1 2 //r=row
-  #define r2 3
-  #define r3 4
-  #define r4 5
-  #define r5 6
-  #define r6 7
-  #define r7 8
-  #define r8 9
-  #define c1 10 //c=column
-  #define c2 11
-  #define c3 12
-  #define c4 13
-  #define c5 14
-  #define c6 15
-  #define c7 16
-  #define c8 17
+  //create object to us matrix functions
+  Matrix myMatrix;
 
-//collect rows and columns
+  //collect rows and columns
   int row[] = {r1, r2 ,r3 ,r4 ,r5, r6, r7, r8};
   int column[] = {c1, c2, c3, c4, c5, c6, c7, c8};
   //define the playfield
   byte field[] = {0x24, 0x24, 0xff, 0x24, 0x24, 0xff, 0x24, 0x24};
 
-//define a point struct
-  typedef struct {
-    int row;
-    int column;
-  }point;
   
+
   //define positions
   point pos1 = {r1, c1};
   point pos2 = {r1, c4};
@@ -57,8 +39,8 @@
 
 void setup() {
   // put your setup code here, to run once:
-  set_pins();
-  clearDisplay();
+  myMatrix.setPins(row, column);
+  //myMatrix.clearDisplay(row, column);
   pinMode(ledPin, OUTPUT);  //Den LED Pin als Ausgang deklarieren.
   pinMode(irPin, INPUT);  //Den IR Pin als Eingang deklarieren.
   irrecv.enableIRIn(); //Den IR Pin aktivieren
@@ -67,7 +49,7 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  drawDisplay(field);
+  myMatrix.drawDisplay(field, row, column);
   remote();
 }
 

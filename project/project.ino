96e71216b10aeca66b21c713a2f8095d4a1f818c
Clemens <95419199+cl-02@users.noreply.github.com>
2022-11-02 16:34:06 +0100

    add solution of remote

--- project/project.ino
+++ project/project.ino
@@ -1,6 +1,7 @@
-
+#include <IRremote.h>
 #include "src/classes/Matrix/matrix.h"
 
+#define irPin 22 // Remote pin
   //create object to us matrix functions
   Matrix myMatrix;
 
@@ -23,7 +24,7 @@
   point pos8 = {r7, c4};
   point pos9 = {r7, c7};
 
-  int irPin = 22; //Pin am Arduino Nano für den IR Receiver
+  
   int ledPin =24; //Pin am Arduino Nano für die LED als Signalbestätigung
 
   int led1Pin = 26; //Pin am Arduino Nano für LED 1
@@ -32,8 +33,28 @@
   boolean led1Status = false;
   boolean led2Status = false;
 
-
+struct Button
+{
+    boolean But0 = false;
+    boolean But1 = false;
+    boolean But2 = false;
+    boolean But3 = false;
+    boolean But4 = false;
+    boolean But5 = false;
+    boolean But6 = false;
+    boolean But7 = false;
+    boolean But8 = false;
+    boolean But9 = false;
+    boolean ON = false;
+    boolean Mode = false;
+    boolean Plus = false;
+    boolean Minus = false;
+};
  
+
+Button button;
+IRrecv irrecv(irPin); //Objekt initialisieren für die IR Übertragung
+decode_results results;
 
 
 void setup() {
@@ -41,14 +62,18 @@ void setup() {
   myMatrix.setPins(row, column);
   //myMatrix.clearDisplay(row, column);
   pinMode(ledPin, OUTPUT);  //Den LED Pin als Ausgang deklarieren.
-  pinMode(irPin, INPUT);  //Den IR Pin als Eingang deklarieren.
  
+  // code for remote
+  pinMode(irPin, INPUT);  //Den IR Pin als Eingang deklarieren.
+  irrecv.enableIRIn(); //Den IR Pin aktivieren
+  Serial.begin(9600); //Serielle kommunikation mit 9600 Baud beginnen.
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   myMatrix.drawDisplay(field, row, column);
   
+  remote(results, button);
 }
 
 

74ef04691e5a33f01d1af1f10f82adfa09e4bb20
Clemens <95419199+cl-02@users.noreply.github.com>
2022-11-24 14:18:51 +0100

    add comments

--- project/Remote.ino
+++ project/Remote.ino
@@ -4,7 +4,7 @@ int remote(decode_results results)
   if (irrecv.decode(&results)) { //Wenn etwas gelesen wurde dann...
       //Ausgabe des Wertes auf die Serielle Schnittstelle.
       int value = results.value;
-      irrecv.resume();
+      irrecv.resume();          // warte auf Werte von Fernbedienung
       switch (value)
       {
           case 26775: //Taste 0
